a09c076f074be7dd47385831811c9c34c0834217
Alex <samokhinteam@gmail.com>
2025-01-23 17:38:31 +0800

    dynamic block, editor and the server side render component

--- build/blocks/term-query/render.php
+++ build/blocks/term-query/render.php
@@ -2,6 +2,11 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+ $wrapper_classes = 'wp-block-term-query';
+$output = '<div ' . get_block_wrapper_attributes( array( 'class' => $wrapper_classes ) ) . '>';
+$output .= '<p>' . esc_html__( 'Term Query – hello from a dynamic block!', 'term-query' ) . '</p>';
+echo $output;
 ?>
 <p <?php echo get_block_wrapper_attributes(); ?>>
 	<?php esc_html_e( 'Term Query – hello from a dynamic block!', 'term-query' ); ?>
